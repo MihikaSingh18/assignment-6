58f424fda749a7dc09058a44b615ad66b395039e
MihikaSingh18 <138354730+MihikaSingh18@users.noreply.github.com>
2023-07-26 21:40:23 +0530

    Add files via upload

---
+++ assignment 6.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      singh
+#
+# Created:     26-07-2023
+# Copyright:   (c) singh 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+name="mihika singh"
+print(name.upper())
+print(name)
+
+name="MIHIKA SINGH"
+print(name.lower())
+print(name)
